893a83f2a319b4f36dc03d487cfe0e8f25c12ff3
Sachin Kamath <kamathsachin@hotmail.com>
2021-11-22 20:56:27 -0600

    Created Date component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,9 +1,10 @@
 import './ExpenseItem.css';
+import ExpenseDate from './ExpenseDate';
 
 const ExpenseItem = ({ expenseAmt, expenseDesc, expenseDate }) => {
   return (
     <div className="expense-item">
-      <div>{expenseDate.toString()}</div>
+      <ExpenseDate expenseDate={expenseDate} />
       <div className="expense-item__description">
         <h2>{expenseDesc}</h2>
         <div className="expense-item__price">${expenseAmt}</div>
