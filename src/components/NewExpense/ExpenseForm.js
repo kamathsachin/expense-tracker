2884ed712366ff6ae313ba7e63df78bee2b15058
Sachin Kamath <kamathsachin@hotmail.com>
2021-11-25 11:22:13 -0600

    Implemented 2 way binding to clear the data after submission

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,21 +1,68 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './ExpenseForm.css';
 
 const ExpenseForm = () => {
+  const [newExpenseDesc, setNewExpenseDesc] = useState('');
+  const [newExpenseAmt, setNewExpenseAmt] = useState('');
+  const [newExpenseDt, setNewExpenseDt] = useState('');
+
+  const descChangeHandler = (event) => {
+    setNewExpenseDesc(event.target.value);
+  };
+
+  const amtChangeHandler = (event) => {
+    setNewExpenseAmt(event.target.value);
+  };
+
+  const dateChangeHandler = (event) => {
+    setNewExpenseDt(event.target.value);
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const expenseData = {
+      desc: newExpenseDesc,
+      amount: newExpenseAmt,
+      date: new Date(newExpenseDt),
+    };
+
+    console.log(expenseData);
+
+    setNewExpenseDesc('');
+    setNewExpenseAmt('');
+    setNewExpenseDt('');
+  };
+
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Expense Desc.</label>
-          <input type="text"></input>
+          <input
+            type="text"
+            value={newExpenseDesc}
+            onChange={descChangeHandler}
+          ></input>
         </div>
         <div className="new-expense__control">
           <label>Expense Amount</label>
-          <input type="text"></input>
+          <input
+            type="text"
+            min="0.01"
+            step="0.01"
+            value={newExpenseAmt}
+            onChange={amtChangeHandler}
+          ></input>
         </div>
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" min="2019-01-01" max="2022-12-31"></input>
+          <input
+            type="date"
+            min="2019-01-01"
+            max="2022-12-31"
+            value={newExpenseDt}
+            onChange={dateChangeHandler}
+          ></input>
         </div>
       </div>
       <div className="new-expense__actions">
